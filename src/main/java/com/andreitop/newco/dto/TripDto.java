55988c46c93c00a65b3f9e05b248c7eaa2a06ce8
Dmitry <euroshow.light.demyanov@gmail.com>
2018-09-07 15:02:46 +0300

    validation constraints for dto

--- src/main/java/com/andreitop/newco/dto/TripDto.java
+++ src/main/java/com/andreitop/newco/dto/TripDto.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.*;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,8 +14,22 @@ public class TripDto implements DTO {
 
     @JsonIgnore
     private static final long serialVersionUID = 5914366185889783660L;
+
+    @NotNull
+    @Min(0L)
     private Long id;
+
+    @NotEmpty
+    @Pattern(regexp = "[A-Z]{1,3}")
     private String origin;
+
+    @NotEmpty
+    @Pattern(regexp = "[A-Z]{1,3}")
     private String destination;
+
+    @NotNull
+    @Positive
+    @Min(100)
+    @Max(100000)
     private Integer price;
 }
