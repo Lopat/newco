8dad1b996ac459531316689ae854412480e09b9c
Dmitry <euroshow.light.demyanov@gmail.com>
2018-09-07 14:54:17 +0300

    trip service tests done

--- src/main/java/com/andreitop/newco/dto/TripDto.java
+++ src/main/java/com/andreitop/newco/dto/TripDto.java
@@ -1,9 +1,13 @@
 package com.andreitop.newco.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TripDto implements DTO {
 
     @JsonIgnore
--- src/test/java/com/andreitop/newco/service/TripServiceTest.java
+++ src/test/java/com/andreitop/newco/service/TripServiceTest.java
@@ -10,9 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
+import static java.util.Optional.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -38,27 +43,48 @@ public class TripServiceTest {
 
     @Test
     public void save() {
+
         tripService.save(testList.get(0));
         verify(tripRepository, times(1)).save(testList.get(0));
     }
 
     @Test
     public void update() {
+
         tripService.update(testList.get(0));
         verify(tripRepository, times(1)).update(testList.get(0));
     }
 
     @Test
     public void deleteById() {
+
         tripService.deleteById(1L);
         verify(tripRepository, times(1)).deleteById(1L);
     }
 
     @Test
     public void findById() {
+
+        TripDto expected = new TripDto(1L, "ORI", "DES", 123);
+        given(tripRepository.findById(1L)).willReturn(expected);
+
+        TripDto resultTrip = tripService.findById(1L);
+        assertEquals(of(1L), ofNullable(resultTrip.getId()));
+        assertEquals("ORI", resultTrip.getOrigin());
+        assertEquals("DES", resultTrip.getDestination());
+        assertEquals(of(123), ofNullable(resultTrip.getPrice()));
     }
 
     @Test
     public void findAll() {
+
+        List<TripDto> newList = new ArrayList <>();
+        newList.add(new TripDto(2L, "ORI2", "DES2", 123));
+        newList.add(new TripDto(3L, "ORI3", "DES3", 123));
+        newList.add(new TripDto(4L, "ORI4", "DES4", 123));
+        given(tripRepository.findAll()).willReturn(newList);
+
+        List<TripDto> tripServiceAll = tripService.findAll();
+        assertEquals(3, tripServiceAll.size());
     }
 }
