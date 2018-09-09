aecc590cce04ad1181efb1a5f56e4db850256824
Dmitry <euroshow.light.demyanov@gmail.com>
2018-09-09 16:40:08 +0300

    several advices/pointcuts added, so i suppose done/

--- src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
+++ src/main/java/com/andreitop/newco/aspect/LoggingAspect.java
@@ -4,6 +4,9 @@ package com.andreitop.newco.aspect;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -22,4 +25,21 @@ public class LoggingAspect {
         logger.info(" ---> Method " + className + "." + methodName + " is about to be called");
     }
 
+    @After("com.andreitop.newco.aspect.PointcutContainer.allControllerMethods()")
+    public void logControllersAfter(JoinPoint jp){
+        String className = jp.getSignature().getDeclaringTypeName();
+        String methodName = jp.getSignature().getName();
+        logger.info(" ---> Method " + className + "." + methodName + " has  been called");
+    }
+
+   @Around("com.andreitop.newco.aspect.PointcutContainer.idMethodsInService()")
+    public Object serviceMethodsWithId(ProceedingJoinPoint pjp) throws Throwable {
+        String className = pjp.getSignature().getDeclaringTypeName();
+        String methodName = pjp.getSignature().getName();
+        Long id = (Long) pjp.getArgs()[0];
+        logger.info(" ---> Method " + className + "." + methodName + " is about to be called with argument " + id);
+        Object o = pjp.proceed();
+        logger.info(" ---> Method " + className + "." + methodName + " has  been called");
+        return o;
+    }
 }
--- src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
+++ src/main/java/com/andreitop/newco/aspect/PointcutContainer.java
@@ -5,6 +5,21 @@ import org.aspectj.lang.annotation.Pointcut;
 public class PointcutContainer {
 
     @Pointcut("execution( * com.andreitop.newco.repository.*Repo*.find*())")
-    public void repositoryFind() {
-    }
+    public void repositoryFind() {}
+
+    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
+    public void allControllerMethods() {}
+
+    @Pointcut("@target(org.springframework.stereotype.Service)")
+    public void serviceMethods() {}
+
+    @Pointcut("execution(* *..*(Long))")
+    public void longParamMethods() {}
+
+    @Pointcut("serviceMethods() && longParamMethods()")
+    public void idMethodsInService() {}
+
+//    @Pointcut("@annotation( org.springframework.boot.autoconfigure.SpringBootApplication)")
+//    public void mainClassMethods() {}
 }
+
